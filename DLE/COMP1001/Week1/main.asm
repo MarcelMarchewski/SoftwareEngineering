fa6a075922bbe6370a8527037cea1c7dc6abba7a
mmarchewski <mmarchewski@SMB109-23>
2025-10-03 10:30:50 +0100

    Completed COMP1001 Week 1 Task

--- DLE/COMP1001/Week1/main.asm
+++ DLE/COMP1001/Week1/main.asm
@@ -1,22 +1,25 @@
-; A simple example adding two numbers.
+; Addition and subtraction homework
 .386  ; Specify instruction set
 .model flat, stdcall  ; Flat memory model, std. calling convention
 .stack 4096 ; Reserve stack space
 ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
 
-.data ; data segment
-	; define your variables here
-  sum DWORD 0  ; define a variable sum (32-bit) with initial value 0
-  
-.code ; code segment
+.data
+  result DWORD ?
+.code
+main PROC
+  mov eax, 15 ; store 'A'
+  mov ebx, 20 ; store 'B'
+  mov ecx, 5 ; store 'C'
+  mov edx, 10 ; store 'D'
 
-main PROC ; main procedure
-	; write your assembly code here
-  mov eax, 25 ; move literal value 25 to eax
-  mov ebx, 30 ; move literal value 30 to ebx
-  add eax, ebx  ; add eax and ebx, and put value in eax
-  mov sum, eax  ; move eax value to sum
+  add eax, ebx ; add 'A' and 'B', store result in eax
+  add ecx, edx ; add 'C' and 'D', store result in ebx
+
+  sub eax, edx ; subtract '(A+B)' and '(C+D)', store result in eax
+
+  mov result, eax ; move '(A+B) - (C+D)' to result variable
 	INVOKE ExitProcess, 0 ; call exit function
   
-main ENDP ; exit main procedure
-END main  ; stop assembling
+main ENDP
+END main
