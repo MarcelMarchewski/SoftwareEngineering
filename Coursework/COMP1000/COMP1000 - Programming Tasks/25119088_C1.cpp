d2990e78a21d37afab1f52c86d2c6d0a42a6cc8a
Marcel Marchewski <marcel.marchewski@students.plymouth.ac.uk>
2025-10-10 12:27:33 +0100

    Improved COMP1000 coursework outputs

--- Coursework/COMP1000/COMP1000 - Programming Tasks/25119088_C1.cpp
+++ Coursework/COMP1000/COMP1000 - Programming Tasks/25119088_C1.cpp
@@ -12,11 +12,9 @@ const int STUDENT_ID = 25119088;
 /// </summary>
 void task1() 
 {
-	// Declare and initialise each number with the last two digits of my student ID
 	int numOne = 8;
 	int numTwo = 8;
 
-	// Output the sum of both integers to the console
 	cout << numOne + numTwo;
 }
 
@@ -25,8 +23,6 @@ void task1()
 /// </summary>
 void task2()
 {
-	// Since there is no requirement to "read" the two numbers in this task, using variables is unnecessary
-	// Multiply the first digit of my student ID with 7 and output the result directly to the console
 	cout << 2 * 7;
 }
 
@@ -36,8 +32,6 @@ void task2()
 /// </summary>
 void task3() 
 {
-	// The max() call ensures that if STUDENT_ID % 20 is not greater than 10, every number between 1-10 is still displayed to the console
-	// In the case of my student ID, this max() call will always return 10 because STUDENT_ID % 20 = 8, but this avoids hard coding
 	for (int i = 1; i <= max(10, STUDENT_ID % 20); i++) 
 	{
 		cout << i << "\n";
@@ -49,9 +43,6 @@ void task3()
 /// </summary>
 void task4() 
 {
-	// Performing % 2 on any integer will return the remainder when that integer is divided by two
-	// Integers that are divisible by two are even and will return 0
-	// Integers that are not divisible by two are odd and will return 1
 	if (STUDENT_ID % 2 == 0) 
 	{
 		cout << STUDENT_ID << " is even";
@@ -68,7 +59,15 @@ void task4()
 /// </summary>
 void task5() 
 {
-	switch ((STUDENT_ID % 7) + 1) 
+	int choice;
+
+	cout << "Enter a number between 1-7: ";
+
+	cin >> choice;
+
+	// (Student ID % 7) + 1
+
+	switch (choice) 
 	{
 		case (1): 
 		{
@@ -111,6 +110,11 @@ void task5()
 			cout << "Sunday";
 			break;
 		}
+
+		default: 
+		{
+			cout << "Invalid input!";
+		}
 	}
 }
 
@@ -119,10 +123,8 @@ void task5()
 /// </summary>
 void task6()
 {
-	// Declare and initialise array with size of 5 and custom values, including student ID personalisation
-	int arr[5] = { STUDENT_ID % 50, 6, 1, 9, 8 };
+	int arr[5] = { 7, 6, STUDENT_ID % 50, 9, 8 };
 
-	// Output the third element of the array (zero indexed)
 	cout << arr[2];
 }
 
@@ -151,6 +153,33 @@ void task10()
 
 int main() 
 {
-	task1(); task2(); task3(); task4(); task5(); task6(); task7(); task8(); task9(); task10();
+	// Move each task function into an array
+	void* ptrs[10] { task1, task2, task3, task4, task5, task6, task7, task8, task9, task10 };
+
+	// Declare and initialise index variable
+	int i = 0;
+
+	// Iterate through each function pointer
+	for (void* t : ptrs) 
+	{
+		// Clear the screen
+		system("cls");
+
+		// Output task number
+		cout << "Task " << i + 1 << ":\n\n";
+
+		// Call the current task's function
+		((void(*)())t)();
+
+		// Output completion message
+		cout << "\n\nTask " << i + 1 << " Complete\n";
+
+		// Wait for any key to be pressed
+		system("pause");
+
+		// Increment the index variable
+		i++;
+	}
+
 	return 0;
 }
