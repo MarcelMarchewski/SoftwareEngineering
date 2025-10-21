7bd570ec7651ac9eb855688be3883a4fbc847325
MarcelMarchewski <marcel.marchewski@students.plymouth.ac.uk>
2025-10-21 11:12:53 +0100

    Set up COMP1001 - Assessment 1 Project layout

---
+++ Coursework/COMP1001/COMP1001 - Assessment 1/q2.asm
@@ -0,0 +1,25 @@
+; Addition and subtraction homework
+.386  ; Specify instruction set
+.model flat, stdcall  ; Flat memory model, std. calling convention
+.stack 4096 ; Reserve stack space
+ExitProcess PROTO, dwExitCode: DWORD  ; Exit process prototype
+
+.data
+  result SDWORD ? ; define result variable
+.code
+main PROC
+  mov eax, 15 ; store 'A'
+  mov ebx, 20 ; store 'B'
+  mov ecx, 5 ; store 'C'
+  mov edx, 10 ; store 'D'
+
+  add eax, ebx ; add 'A' and 'B', store result in eax
+  add ecx, edx ; add 'C' and 'D', store result in ebx
+
+  sub eax, edx ; subtract '(A+B)' and '(C+D)', store result in eax
+
+  mov result, eax ; move '(A+B) - (C+D)' to result variable
+	INVOKE ExitProcess, 0 ; call exit function
+  
+main ENDP
+END main
