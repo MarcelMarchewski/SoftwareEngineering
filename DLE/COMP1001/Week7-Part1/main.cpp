81810a829a82c81a7fcf234274d41ad91b2b6e17
Marcel Marchewski <marcel.marchewski@students.plymouth.ac.uk>
2025-11-14 15:16:08 +0000

    Week 7 COMP1001

---
+++ DLE/COMP1001/Week7-Part1/main.cpp
@@ -0,0 +1,75 @@
+/*
+------------------DR VASILIOS KELEFOURAS-----------------------------------------------------
+------------------COMP1001 ------------------------------------------------------------------
+------------------COMPUTER SYSTEMS MODULE-------------------------------------------------
+------------------UNIVERSITY OF PLYMOUTH, SCHOOL OF ENGINEERING, COMPUTING AND MATHEMATICS---
+*/
+
+#include <Windows.h>
+#include "array_constant_addition.h"
+#include <omp.h> //this library is needed for the timer
+
+#define TIMES_TO_RUN 1000 //how many times the function will run
+
+
+int main() {
+
+	unsigned short int output;
+	int t;
+
+	//define the timers measuring execution time
+	double start_1, end_1;
+
+	//the following command pins the current process to the 1st core
+	//otherwise, the OS tongles this process between different cores
+	BOOL success = SetProcessAffinityMask(GetCurrentProcess(), 1);
+	if (success == 0) {
+		//cout << "SetProcessAffinityMask failed" << endl;
+		printf("\nSetProcessAffinityMask failed\n");
+		system("pause");
+		return -1;
+	}
+
+	//initialize the arrays
+	initialization_ConstAdd();
+
+	start_1 = omp_get_wtime(); //start the timer 
+
+	//auto start = std::chrono::high_resolution_clock::now(); //ACCURATE timer provided in C++ only
+
+	//run this several times because this routine runs very fast 
+	//The execution time needs to be at least some seconds in order to have a good measurement (why?) 
+	//			because other processes run at the same time too, preempting our thread
+	//---Appropriately MODIFY the 'TIMES_TO_RUN' and the input size (defined in the appropriate header file)---
+	for (t = 0; t < TIMES_TO_RUN; t++) {
+
+		//output = ConstAdd_default();
+		//output = ConstAdd_SSE();
+		output = ConstAdd_AVX();
+
+
+	}
+
+
+
+	//auto finish = std::chrono::high_resolution_clock::now(); 
+	end_1 = omp_get_wtime(); //start the timer 
+
+
+	if (Compare_ConstAdd() == 0)
+		printf("\n\n\r ----- output is correct -----\n\r");
+	else
+		printf("\n\n\r -----output is INcorrect -----\n\r");
+
+	printf(" Time in seconds is %f\n", end_1 - start_1);//print the ex.time
+	//std::chrono::duration<double> elapsed = finish - start;
+	//std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+
+	system("pause"); //this command does not let the output window to close
+	return 0;
+}
+
+
+
+
+
---
+++ DLE/COMP1001/Week7-Part2/array_addition.cpp
@@ -0,0 +1,105 @@
+#include "array_addition.h"
+
+float  X1[M2], X2[M2], Y1[M2], test2[M2];
+
+void initialization_Add() {
+
+	float e = 0.1234f, p = 0.7264f, r = 0.11f;
+
+	for (unsigned int j = 0; j != M2; j++) {
+		Y1[j] = 0.0;
+		test2[j] = 0.0;
+		X1[j] = (j % 7) + r;
+		X2[j] = (j % 13) + e;
+	}
+}
+
+
+unsigned short int Add_default() {
+
+	for (int j = 0; j < M2; j++) 
+	{
+		Y1[j] = X1[j] + X2[j];
+	}
+
+	return 0;
+}
+
+unsigned short int Add_SSE() {
+
+	__m128 num1, num2, num3;
+
+	for (int i = 0; i < M2; i += 4) 
+	{
+		num1 = _mm_loadu_ps(&X1[i]);
+		num2 = _mm_loadu_ps(&X2[i]);
+
+		num3 = _mm_add_ps(num1, num2);
+
+		_mm_storeu_ps(&Y1[i], num3);
+	}
+
+	return 0;
+}
+
+unsigned short int Add_SSE_all_sizes() 
+{
+	__m128 num1, num2, num3;
+
+	int i;
+
+	for (i = 0; i < (M2 / 4) * 4; i += 4) 
+	{
+		num1 = _mm_loadu_ps(&X1[i]);
+		num2 = _mm_loadu_ps(&X2[i]);
+		
+		num3 = _mm_add_ps(num1, num2);
+
+		_mm_storeu_ps(&Y1[i], num3);
+	}
+
+	for (; i < M2; i++) 
+	{
+		Y1[i] = X1[i] + X2[i];
+	}
+
+	return 0;
+}
+
+
+unsigned short int Add_AVX() {
+
+	__m256  ymm1, ymm2, ymm3;
+
+	for (int i = 0; i < M2; i += 8) 
+	{
+		ymm1 = _mm256_loadu_ps(&X1[i]);
+		ymm2 = _mm256_loadu_ps(&X2[i]);
+
+		ymm3 = _mm256_add_ps(ymm1, ymm2);
+
+		_mm256_storeu_ps(&Y1[i], ymm3);
+	}
+
+	return 0;
+}
+
+
+unsigned short int Compare_Add() {
+
+
+	for (int j = 0; j < M2; j++) {
+		test2[j] = X1[j] + X2[j];
+	}
+
+
+	for (int j = 0; j < M2; j++)
+		if (equal(Y1[j], test2[j]) == 1) {
+			//printf("\n j=%d\n", j);
+			return 1;
+		}
+
+	return 0;
+}
+
+
